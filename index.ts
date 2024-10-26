d607280a428d1d8a94b5f470e85a309e4c4a868d
TIMI <Akinolae>
2024-10-26 15:10:43 +0100

    added TTL option for flexibility

--- index.ts
+++ index.ts
@@ -1,45 +1,46 @@
 enum Intervals {
-    // Query every 1 minute
-    Query = 1 * 60 * 1000,
+  // Query every 1 minute
+  Query = 1 * 60 * 1000,
 }
 
 type IntervalStore = {
-    callID: string;
-    lastFetch: number | string;
+  callID: string;
+  lastFetch: number | string;
 };
 
 type IntervalStoreObject = {
-    [key: string]: Array<IntervalStore>;
+  [key: string]: Array<IntervalStore>;
 };
 
 let interval_list: IntervalStoreObject;
 
 /**
- * @param {*} params
+ * @param {*} id
  * @returns a single call that matches the provided ID
  * checks if the call id exists in the interval-list
  */
 
-const findCallByID = (params: { callId: string, functionId: string }): IntervalStore | undefined => {
+const findCallByID = (params: {
+  callId: string;
+  functionId: string;
+}): IntervalStore | undefined => {
+  const { callId, functionId } = params;
+  if (!callId || !functionId) {
+    throw new Error("INTERVAL_CACHE_ERROR:CallId and functionId is required");
+  }
 
-    const { callId, functionId } = params
-    if (!callId || !functionId) {
-        throw new Error('INTERVAL_CACHE_ERROR:CallId and functionId is required');
+  const intervalExists =
+    interval_list !== undefined ? interval_list[callId] : [];
+  if (Array.isArray(intervalExists) && intervalExists.length > 0) {
+    for (const interval of intervalExists) {
+      if (interval.callID === functionId) {
+        return interval;
+      } else {
+        return;
+      }
     }
-
-    const intervalExists =
-        interval_list !== undefined ? interval_list[callId] : [];
-    if (Array.isArray(intervalExists) && intervalExists.length > 0) {
-        for (const interval of intervalExists) {
-            if (interval.callID === functionId) {
-                return interval
-            } else {
-                return
-            }
-
-        }
-        return interval_list[callId].find((r) => r.callID === functionId);
-    }
+    return interval_list[callId].find((r) => r.callID === functionId);
+  }
 };
 
 /**
@@ -48,63 +49,71 @@ const findCallByID = (params: { callId: string, functionId: string }): IntervalS
  * checks if the call id exists in the interval-list
  */
 
-const addCallToList = (params: { callid: string, intervalObject: IntervalStore }) => {
-    const { callid, intervalObject } = params;
+const addCallToList = (params: {
+  callid: string;
+  intervalObject: IntervalStore;
+}) => {
+  const { callid, intervalObject } = params;
 
-    // Check that the callid exists before performing any mutation
-    const callidExists = Object.keys(interval_list || {}).some(
-        (id) => id === callid,
-    );
+  // Check that the callid exists before performing any mutation
+  const callidExists = Object.keys(interval_list || {}).some(
+    (id) => id === callid
+  );
 
-    if (!callidExists) {
-        interval_list = {
-            ...interval_list,
-            [callid]: [intervalObject],
-        };
-    } else {
-        const currentInterval = [
-            ...interval_list[callid].filter(
-                (a) => a?.callID !== intervalObject.callID,
-            ),
-            intervalObject,
-        ];
+  if (!callidExists) {
+    interval_list = {
+      ...interval_list,
+      [callid]: [intervalObject],
+    };
+  } else {
+    const currentInterval = [
+      ...interval_list[callid].filter(
+        (a) => a?.callID !== intervalObject.callID
+      ),
+      intervalObject,
+    ];
 
-        interval_list = {
-            ...interval_list,
-            [callid]: currentInterval,
-        };
-    }
+    interval_list = {
+      ...interval_list,
+      [callid]: currentInterval,
+    };
+  }
 
-    return interval_list;
+  return interval_list;
 };
 
 /**
- * @param {*} callID
+ * @param {*} params
  * checks if the call id exists in the interval-list
  */
-const intervalCheck = (params: { callId: string, functionId: string }): boolean => {
-    const { callId, functionId } = params
-    const callIDexists = findCallByID({ callId, functionId });
+const intervalCheck = (params: {
+  callId: string;
+  functionId: string;
+  ttl?: number;
+}): boolean => {
+  const { callId, functionId, ttl } = params;
 
-    const date = new Date();
+  const intervalTime = ttl ?? Intervals.Query;
+  const callIDexists = findCallByID({ callId, functionId });
+  const date = new Date();
 
-    if (
-        callIDexists === undefined ||
-        !callIDexists.lastFetch ||
-        date.getTime() - +callIDexists.lastFetch > Intervals.Query
-    ) {
-        interval_list = addCallToList({
-            callid: callId,
-            intervalObject: {
-                lastFetch: date.getTime(),
-                callID: functionId,
-            },
-        });
+  if (
+    callIDexists === undefined ||
+    !callIDexists.lastFetch ||
+    date.getTime() - +callIDexists.lastFetch > intervalTime
+  ) {
+    interval_list = addCallToList({
+      callid: callId,
+      intervalObject: {
+        lastFetch: date.getTime(),
+        callID: functionId,
+      },
+    });
 
-        return true;
-    } else {
-        return false;
-    }
+    return true;
+  } else {
+    return false;
+  }
 };
 
 export { intervalCheck };
